034030999ebe679560f175e00c222d8f8884f155
meta-prakshat-jain <prakshat.jain@metacube.com>
2025-03-12 22:31:14 +0530

    MathsTest.java

--- src/test/java/com/metacube/junit/JUnitAssignment/MathsTest.java
+++ src/test/java/com/metacube/junit/JUnitAssignment/MathsTest.java
@@ -23,32 +23,32 @@ public class MathsTest {
 	}
 	@Test
 	public void LCMusingGCDTest() {
-			assertEquals(54,math.LCMusingGCD(54, 1));
-			assertEquals(6, math.LCMusingGCD(2, 3));
-			assertEquals(12, math.LCMusingGCD(4,6));
-			assertEquals(30, math.LCMusingGCD(10, 15));
-			assertEquals(16, math.LCMusingGCD(8, 16));
-			assertEquals(12, math.LCMusingGCD(12, 3));
-			assertEquals(63, math.LCMusingGCD(7, 9));
-			assertEquals(143, math.LCMusingGCD(11, 13));
-			assertEquals(500, math.LCMusingGCD(100, 250));
-			assertEquals(0, math.LCMusingGCD(0, 5));
-			assertEquals(7, math.LCMusingGCD(1, 7));
-			assertEquals(12, math.LCMusingGCD(-4, 6));
+		assertEquals(54,math.LCMusingGCD(54, 1));
+		assertEquals(6, math.LCMusingGCD(2, 3));
+		assertEquals(12, math.LCMusingGCD(4,6));
+		assertEquals(30, math.LCMusingGCD(10, 15));
+		assertEquals(16, math.LCMusingGCD(8, 16));
+		assertEquals(12, math.LCMusingGCD(12, 3));
+		assertEquals(63, math.LCMusingGCD(7, 9));
+		assertEquals(143, math.LCMusingGCD(11, 13));
+		assertEquals(500, math.LCMusingGCD(100, 250));
+		assertEquals(0, math.LCMusingGCD(0, 5));
+		assertEquals(7, math.LCMusingGCD(1, 7));
+		assertEquals(12, math.LCMusingGCD(-4, 6));
 	}
 	@Test
 	public void HcfUsingGcdTest() {
-			assertEquals(1,math.HCFusingGCD(54, 1));
-			assertEquals(1, math.HCFusingGCD(2, 3));
-			assertEquals(2, math.HCFusingGCD(4,6));
-			assertEquals(5, math.HCFusingGCD(10, 15));
-			assertEquals(8, math.HCFusingGCD(8, 16));
-			assertEquals(3, math.HCFusingGCD(12, 3));
-			assertEquals(1, math.HCFusingGCD(7, 9));
-			assertEquals(1, math.HCFusingGCD(11, 13));
-			assertEquals(5, math.HCFusingGCD(0, 5));
-			assertEquals(1, math.HCFusingGCD(1, 7));
-			assertEquals(2, math.HCFusingGCD(-4, 6));
+		assertEquals(1,math.HCFusingGCD(54, 1));
+		assertEquals(1, math.HCFusingGCD(2, 3));
+		assertEquals(2, math.HCFusingGCD(4,6));
+		assertEquals(5, math.HCFusingGCD(10, 15));
+		assertEquals(8, math.HCFusingGCD(8, 16));
+		assertEquals(3, math.HCFusingGCD(12, 3));
+		assertEquals(1, math.HCFusingGCD(7, 9));
+		assertEquals(1, math.HCFusingGCD(11, 13));
+		assertEquals(5, math.HCFusingGCD(0, 5));
+		assertEquals(1, math.HCFusingGCD(1, 7));
+		assertEquals(2, math.HCFusingGCD(-4, 6));
 		
 	}
 	
